05b0dbb19f173ec1c1e128570bb60d5e27643277
mrshappy0 <ajs11300@gmail.com>
2020-04-01 11:17:37 -0700

    Added password_digest attribute to users table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_171139) do
+ActiveRecord::Schema.define(version: 2020_04_01_181646) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_03_31_171139) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
