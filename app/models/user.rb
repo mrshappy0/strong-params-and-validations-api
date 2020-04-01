0644dd6b650db956fb163c2af612a2a64ccf9262
mrshappy0 <ajs11300@gmail.com>
2020-04-01 11:18:58 -0700

    Added 'has_secure_password' to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
   validates :name, :username, :email, presence: true
   validates :username, length: { minimum: 6, maximum: 12 }
   validates :username, uniqueness: true
